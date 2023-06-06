1c3815015d85fe5371958148c748cae35ed315eb
vientokk <vientokk@gmail.com>
2023-06-06 17:24:54 +0900

    Add icon.png file and push button with handler function in maim.py

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 # ch 4.2.1 mail.py
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+							QMessageBox)
+										 
+from PyQt5.QtGui import QIcon
 
 
 class Calculator(QWidget):
+	
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Calculator')
-        self.resize(256, 256)
+        self.btn1=QPushButton('Message',self)
+        self.btn1.clicked.connect(self.activateMessage)
+        
+
+        vbox=QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('Calculator')											 
+        self.setWindowIcon(QIcon('icon.png'))
+        self.resize(256,256)
         self.show()
 
+    def activateMessage(self):
+        QMessageBox.information(self,"information","Button clicked!")
 
+
+        
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    view = Calculator()
+    app=QApplication(sys.argv)
+    view=Calculator()
     sys.exit(app.exec_())
+    

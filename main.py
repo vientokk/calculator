ff32d9af2d7461dcf1b8e3353e51a343de269b8b
vientokk <vientokk@gmail.com>
2023-06-06 17:28:12 +0900

    Add text edit  te1 and fix btn1 handler function

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
-# ch 4.2.1 mail.py
-
+# ch 4.2.4 mail.py
 import sys
-from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-							QMessageBox)
-										 
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
+                             QMessageBox, QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 
 
 class Calculator(QWidget):
-	
+
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.btn1=QPushButton('Message',self)
-        self.btn1.clicked.connect(self.activateMessage)
-        
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
 
-        vbox=QVBoxLayout()
-        vbox.addStretch(1)
+        self.btn1 = QPushButton('Message', self)
+        self.btn1.clicked.connect(self.activateMessage)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.te1)
         vbox.addWidget(self.btn1)
         vbox.addStretch(1)
 
         self.setLayout(vbox)
 
-        self.setWindowTitle('Calculator')											 
+        self.setWindowTitle('Calculator')
         self.setWindowIcon(QIcon('icon.png'))
-        self.resize(256,256)
+        self.resize(256, 256)
         self.show()
 
     def activateMessage(self):
-        QMessageBox.information(self,"information","Button clicked!")
+        # QMessageBox.information(self, "information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
 
 
-        
 if __name__ == '__main__':
-    app=QApplication(sys.argv)
-    view=Calculator()
+    app = QApplication(sys.argv)
+    view = Calculator()
     sys.exit(app.exec_())
-    
